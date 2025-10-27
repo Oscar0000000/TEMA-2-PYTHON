3303287725f8f9f80964beed25c5cd97487b2487
Oscar0000000 <oscaramcort13@gmail.com>
2025-10-27 13:46:34 +0100

    Add files via upload

---
+++ ejer2.py
@@ -0,0 +1,9 @@
+#LISTA DE 5 NUMEROS Y USAR BUCLE SIMPLE
+suma = 0
+elemento = 0
+a = [1,2,3,4,5]
+for i in range(5):
+    elemento = a[i]
+    print(elemento)
+    suma = suma + elemento
+print("La suma es:", suma)
