fd0eea8979f78e2ffdca6e4e95ba6952cb32637d
Oscar0000000 <oscaramcort13@gmail.com>
2025-12-01 16:36:19 +0100

    Add files via upload

---
+++ ejer 23.py
@@ -0,0 +1,14 @@
+p = [[5.0,10.5],[8.2,15.0],[1.5,3.0]]
+suma = 0
+for i in range(len(p)):
+    suma += p[i][1]
+print(f"La suma es:{suma}")
+
+suma = 0
+i = 0
+while i < len(p):
+    suma = suma + p[i][1]
+    i = i + 1
+print(f"La suma es:{suma}")
+
+        
